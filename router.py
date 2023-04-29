0a316313977bd9e716dcd6354ec5895ca248693c
cosmicoppai <kanakchaudhari12@gmail.com>
2023-04-29 17:15:25 +0530

    added route to serve image

--- router.py
+++ router.py
@@ -63,3 +63,12 @@ async def create_user_data(user_data: BasicDetails = Depends(),
     session.commit()
 
     return JSONResponse({"msg": "user data successfully added"}, status_code=status.HTTP_201_CREATED)
+
+@user_router.get(StaticFilesConfig.file_url+"/{}/{ayur_id}/{file_name}")
+def get_report(ayur_id: str, file_name: str, token_data: UserToken = Depends(validate_token)) -> List[str]:
+    session = DataBase().session
+    if not session.query(UsersData.photo).filter(UsersData.ayur_id == token_data.sub).first():
+        raise exceptions.HTTP_404("File Not Found!")
+
+    file_path = FileConfig.checkup_folder.joinpath(f"{ayur_id}/{file_name}")
+    return FileResponse(file_path)
